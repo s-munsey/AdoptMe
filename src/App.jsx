32849c5d887335ea6810c1b40a6c0f068d1b1893
Shaun Munsey <shaunmunsey@alphafx.co.uk>
2023-02-16 16:12:06 +0000

    usestate hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Peppa" animal="Dog" breed="Pug" />
-      <Pet name="Spot" animal="Cat" breed="Fold" />
-      <Pet name="Peep" animal="Bird" breed="Budgie" />
+      <SearchParams />
     </div>
   );
 };
