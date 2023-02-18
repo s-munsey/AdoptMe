f7aa4118e5ee0a7d63a9785bb3229205afe0cfbf
Shaun Munsey <s_munsey@live.com>
2023-02-18 11:24:37 +0000

    react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import SearchParams from "./SearchParams";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SearchParams from "./SearchParams";
 import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
+      <QueryClientProvider client={queryClient}>
         <header>
           <Link to="/">Adopt Me!</Link>
         </header>
@@ -15,7 +24,7 @@ const App = () => {
           <Route path="/details/:id" element={<Details />} />
           <Route path="/" element={<SearchParams />} />
         </Routes>
-      </div>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
---
+++ src/fetchBreedList.js
@@ -0,0 +1,17 @@
+const fetchBreedsList = async ({ queryKey }) => {
+  const animal = queryKey[1];
+
+  if (!animal) return [];
+
+  const apiRes = await fetch(
+    `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+  );
+
+  if (!apiRes.ok) {
+    throw new Error(`breeds?animal=${animal} fetch not ok`);
+  }
+
+  return apiRes.json();
+};
+
+export default fetchBreedsList;
